ec4b3d242490bfeb25ecb8d0287d5acc941e9dde
Lucas Boti <luke.boti.lorken@gmail.com>
2023-10-10 10:56:26 -0300

    tabela criada, rotas funcionando

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    // Registro de usuário
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('API Token')->accessToken;
+
+        return response()->json(['token' => $token], 201);
+    }
+
+    // Login de usuário
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('API Token')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    }
+}
+
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// ...
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register',[AuthController::class, 'register']);
+
+
+
+// Rotas protegidas pela autenticação
+Route::middleware('auth:api')->group(function () {
+    Route::resource('despesas', 'DespesaController');
 });
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
